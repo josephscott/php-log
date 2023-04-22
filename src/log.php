b2eb1af4c98034327113440120892c2cbfd65f25
Joseph Scott <joseph@josephscott.org>
2023-04-22 15:12:07 -0600

    Make the simple case work

--- src/log.php
+++ src/log.php
@@ -2,5 +2,42 @@
 declare( strict_types = 1 );
 
 class Log {
-	public function __construct() { }
+	public $stringify = 'stringify_var_export';
+
+	public function __construct() {
+	}
+
+	public static function __callStatic( string $method, array $args ) {
+		$log = new Log();
+		$response = $log->send(
+			to: $method,
+			msg: $args[0]
+		);
+
+		return $response;
+	}
+
+	public function stringify_var_export( $msg ) {
+		$out = var_export( $msg, true );
+		return $out;
+	}
+
+	public function send( string $to, $msg ) {
+		$response = [
+			'error' => false,
+			'msg' => ''
+		];
+
+		$make_string = $this->stringify;
+		$msg = $this->$make_string( $msg );
+
+		$to = "send_$to";
+		$response['error'] = $this->$to( $msg );
+
+		return $response;
+	}
+
+	public function send_php( $msg ) {
+		return error_log( $msg, 0 );
+	}
 }
